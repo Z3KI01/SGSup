e6dadc9a083115f4a00dde2efdf10facf05d903c
Ezequiel Walginski <ezequielwalginski@gmail.com>
2024-07-06 19:59:39 -0300

    SGSup

---
+++ UtilitariosSup/fSgSup.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace UtilitariosSup
+{
+    partial class fUtilitarios
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fUtilitarios));
+            this.listBoxArquivos = new System.Windows.Forms.ListBox();
+            this.pBSgMaster = new System.Windows.Forms.PictureBox();
+            this.lblAviso = new System.Windows.Forms.Label();
+            this.lblAviso2 = new System.Windows.Forms.Label();
+            this.lblAviso3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pBSgMaster)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxArquivos
+            // 
+            this.listBoxArquivos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBoxArquivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxArquivos.FormattingEnabled = true;
+            this.listBoxArquivos.ItemHeight = 20;
+            this.listBoxArquivos.Location = new System.Drawing.Point(82, 99);
+            this.listBoxArquivos.Name = "listBoxArquivos";
+            this.listBoxArquivos.Size = new System.Drawing.Size(202, 22);
+            this.listBoxArquivos.TabIndex = 0;
+            this.listBoxArquivos.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxArquivos_MouseDoubleClick);
+            // 
+            // pBSgMaster
+            // 
+            this.pBSgMaster.Image = global::UtilitariosSup.Properties.Resources.logo_master_220x48px;
+            this.pBSgMaster.Location = new System.Drawing.Point(54, 25);
+            this.pBSgMaster.Name = "pBSgMaster";
+            this.pBSgMaster.Size = new System.Drawing.Size(226, 52);
+            this.pBSgMaster.TabIndex = 2;
+            this.pBSgMaster.TabStop = false;
+            // 
+            // lblAviso
+            // 
+            this.lblAviso.AutoSize = true;
+            this.lblAviso.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAviso.ForeColor = System.Drawing.Color.Red;
+            this.lblAviso.Location = new System.Drawing.Point(11, 145);
+            this.lblAviso.Name = "lblAviso";
+            this.lblAviso.Size = new System.Drawing.Size(300, 13);
+            this.lblAviso.TabIndex = 3;
+            this.lblAviso.Text = "*DUPLO CLICK OU F8 PARA INICIAR DOWNLOAD ";
+            // 
+            // lblAviso2
+            // 
+            this.lblAviso2.AutoSize = true;
+            this.lblAviso2.BackColor = System.Drawing.Color.Transparent;
+            this.lblAviso2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAviso2.Location = new System.Drawing.Point(15, 101);
+            this.lblAviso2.Name = "lblAviso2";
+            this.lblAviso2.Size = new System.Drawing.Size(66, 18);
+            this.lblAviso2.TabIndex = 4;
+            this.lblAviso2.Text = "Buscar:";
+            // 
+            // lblAviso3
+            // 
+            this.lblAviso3.AutoSize = true;
+            this.lblAviso3.BackColor = System.Drawing.Color.Transparent;
+            this.lblAviso3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAviso3.Location = new System.Drawing.Point(284, 92);
+            this.lblAviso3.Name = "lblAviso3";
+            this.lblAviso3.Size = new System.Drawing.Size(27, 31);
+            this.lblAviso3.TabIndex = 5;
+            this.lblAviso3.Text = "⬍";
+            // 
+            // fUtilitarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 182);
+            this.Controls.Add(this.lblAviso3);
+            this.Controls.Add(this.lblAviso2);
+            this.Controls.Add(this.lblAviso);
+            this.Controls.Add(this.pBSgMaster);
+            this.Controls.Add(this.listBoxArquivos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "fUtilitarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SG Sup";
+            ((System.ComponentModel.ISupportInitialize)(this.pBSgMaster)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pBSgMaster;
+        public System.Windows.Forms.ListBox listBoxArquivos;
+        private System.Windows.Forms.Label lblAviso;
+        public System.Windows.Forms.Label lblAviso2;
+        public System.Windows.Forms.Label lblAviso3;
+    }
+}
+
---
+++ UtilitariosSup/fSgSup.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace UtilitariosSup
+{
+    public partial class fUtilitarios : Form
+    {
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwn, int attr, int[] attrValue, int attriSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            if (DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4) != 0)
+            {
+                DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+            }
+        }
+
+        private List<DownloadItem> _downloadItems = new List<DownloadItem>();
+
+        public fUtilitarios()
+        {
+            InitializeComponent();
+            LoadDownloadItemsAsync();
+        }
+
+        private async void LoadDownloadItemsAsync()
+        {
+            
+            listBoxArquivos.Items.Clear();
+            listBoxArquivos.Items.Add("CARREGANDO...");
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, "https://sgbr.com.br/acessorestrito/api/utilitario/downloads/listar");
+                    request.Headers.Add("token", "5L/8K//}#$@dwKgf/)XqqQB6ZnrhFHX4qm[Y*=wX&%sxSvU5S;nsBm&U=K=7");
+
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        string errorResponse = await response.Content.ReadAsStringAsync();
+                        MessageBox.Show($"ERRO AO CARREGAR LISTA DE DOWNLOADS: {response.StatusCode} - {errorResponse}", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    var downloadResponse = JsonConvert.DeserializeObject<DownloadResponse>(jsonResponse);
+                    _downloadItems = downloadResponse.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"ERRO AO CARREGAR LISTA DE DOWNLOADS: {ex.Message}", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                Invoke(new Action(() =>
+                {
+                    listBoxArquivos.Items.Clear();
+                    if (_downloadItems.Count > 0)
+                    {
+                        foreach (var item in _downloadItems)
+                        {
+                            if (!string.IsNullOrEmpty(item.Nome))
+                            {
+                                listBoxArquivos.Items.Add(item.Nome);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        listBoxArquivos.Items.Add("NENHUM ITEM ENCONTRADO.");
+                    }
+                }));
+            }
+        }
+
+        private void listBoxArquivos_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBoxArquivos.SelectedIndex != -1)
+            {
+                var selectedItem = _downloadItems[listBoxArquivos.SelectedIndex];
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.FileName = selectedItem.Nome;
+                saveFileDialog.Filter = GetFileFilter(selectedItem.Url);
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    DownloadFileAsync(selectedItem.Url, saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private async void DownloadFileAsync(string url, string filePath)
+        {
+            Floading floading = new Floading();
+            try
+            {
+                Invoke(new Action(() =>
+                {
+                    floading.StartPosition = FormStartPosition.Manual;
+                    floading.Location = new Point(this.Location.X + (this.Width - floading.Width) / 2,
+                                                  this.Location.Y + (this.Height - floading.Height) / 2);
+                    floading.Show();
+                    lblAviso.Text = "    AGUARDE, FINALIZANDO O DOWNLOAD..."; 
+                }));
+                floading.Refresh();
+
+                using (HttpClient client = new HttpClient())
+                using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var totalBytes = response.Content.Headers.ContentLength ?? -1L;
+                    var canReportProgress = totalBytes != -1;
+
+                    using (var contentStream = await response.Content.ReadAsStreamAsync())
+                    using (var fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None, 8192, true))
+                    {
+                        var buffer = new byte[8192];
+                        long totalRead = 0L;
+                        int read;
+
+                        while ((read = await contentStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            await fileStream.WriteAsync(buffer, 0, read);
+                            totalRead += read;
+
+                            if (canReportProgress)
+                            {
+                                var progress = (int)((totalRead * 1d) / (totalBytes * 1d) * 100);
+                                floading.Invoke(new Action(() => { floading.SetProgress(progress); }));
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("DOWNLOAD CONCLUÍDO COM SUCESSO!", "SUCESSO!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"ERRO AO BAIXAR O ARQUIVO: {ex.Message}", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Invoke(new Action(() =>
+                {
+                    floading.Close();
+                    lblAviso.Text = "*DUPLO CLICK OU F8 PARA INICIAR DOWNLOAD"; 
+                }));
+            }
+        }
+
+        // By Zequi :)
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.F8)
+            {
+                if (listBoxArquivos.SelectedIndex != -1)
+                {
+                    var selectedItem = _downloadItems[listBoxArquivos.SelectedIndex];
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.FileName = selectedItem.Nome;
+                    saveFileDialog.Filter = GetFileFilter(selectedItem.Url);
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        DownloadFileAsync(selectedItem.Url, saveFileDialog.FileName);
+                    }
+                }
+
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+        private string GetFileFilter(string url)
+        {
+            string fileExtension = System.IO.Path.GetExtension(url);
+            if (!string.IsNullOrEmpty(fileExtension))
+            {
+                return $"Files (*{fileExtension})|*{fileExtension}";
+            }
+            else
+            {
+                return "All files (*.*)|*.*";
+            }
+        }
+
+        public class DownloadItem
+        {
+            public string Nome { get; set; }
+            public string Url { get; set; }
+        }
+
+        public class DownloadResponse
+        {
+            public List<DownloadItem> Data { get; set; }
+        }
+    }
+}
